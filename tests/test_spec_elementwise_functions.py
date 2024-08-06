45ca3ad1d2a7bc4e3c18cce4c6e577b602bedaae
ohrechykha <alexeee6@gmail.com>
2024-08-06 09:54:24 +0300

    removing mypy.ini and ignoring a particular missing import

--- tests/test_spec_elementwise_functions.py
+++ tests/test_spec_elementwise_functions.py
@@ -25,7 +25,7 @@ has_complex_dtype = True
 if np.lib.NumpyVersion(np.__version__) < '2.0.0b1':
   with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    import array_api_strict as xp
+    import array_api_strict as xp # type: ignore
     has_complex_dtype = np.dtype("complex128") in xp._dtypes._all_dtypes
 else:
   xp = np
